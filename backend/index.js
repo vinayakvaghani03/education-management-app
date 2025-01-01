55f799819c878e2b40b05f8dcf81e9752483ad1a
vinayakvaghani03 <vinayakwork8@gmail.com>
2025-01-02 03:04:22 +0530

    Schema-done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose")
+const Routes = require("./routes/route.js")
 
 dotenv.config();
 app.use(express.json({ limit: "10mb" }))
@@ -11,7 +12,9 @@ app.use(cors())
 // database connection
 
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to the mongoDB"))
-.catch((error)=>console.log("Not Connected to the mongoDB"))
+.catch((error)=>console.log("Not Connected to the mongoDB"));
+
+app.use('/', Routes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started at port no. ${process.env.PORT}`)
