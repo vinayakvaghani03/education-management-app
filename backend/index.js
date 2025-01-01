9532f666a18ffe029bd9bdf510909f87c19fdcb6
vinayakvaghani03 <vinayakwork8@gmail.com>
2025-01-02 02:13:30 +0530

    connect to DB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
-const express=require("express")
-const app=express();
-const dotenv=require("dotenv");
-const cors =require("cors")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose")
 
 dotenv.config();
-app.use(express.json({limit:"10mb"}))
+app.use(express.json({ limit: "10mb" }))
 app.use(cors())
 
-app.listen(process.env.PORT, ()=>{
+// database connection
+
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to the mongoDB"))
+.catch((error)=>console.log("Not Connected to the mongoDB"))
+
+app.listen(process.env.PORT, () => {
     console.log(`Server started at port no. ${process.env.PORT}`)
 })
 
